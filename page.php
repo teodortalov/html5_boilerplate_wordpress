e4a4aa428e81a3ab7741fba7876523bfcda8936c
Teodor Talov <teodor@talov.com>
2013-06-20 16:38:15 -0400

    Added color guides and some css

--- page.php
+++ page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <div id="contentwrap">
-	<div id="content">
+	<article id="content">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
   		<div class="post" id="post-<?php the_ID(); ?>">
   			<h2><?php the_title(); ?></h2>
@@ -10,8 +10,10 @@
   		</div>
 	  <?php endwhile; endif; ?>
 		<?php edit_post_link('Edit', '<p>', '</p>'); ?>
-  </div>
+  </article>
+  <aside>
   <?php get_sidebar(); ?>
   <?php get_sidebar ('right'); ?>
+  </aside>
 </div>
 <?php get_footer(); ?>
